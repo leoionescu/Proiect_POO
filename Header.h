708f2d694ebc542b1418de2d867faec78c7f6a8c
leoionescu <45562033+leoionescu@users.noreply.github.com>
2020-12-07 16:06:54 +0200

    detecteaza comanda

--- Header.h
+++ Header.h
@@ -1,1 +1,92 @@
 #pragma once
+
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+class createTable
+{
+private:
+
+public:
+
+};
+
+int lungimeComanda(char* s)
+{
+	int counter = 0;
+	for (int i = 0; i < strlen(s); i++)
+	{
+		if (s[i] == ' ')
+		{
+			counter++;
+		}
+		if (counter == 2)
+		{
+			return i - 1;
+		}
+	}
+}
+
+bool compara(char* s1, const char* s2)
+{
+	for (int i = 0; i < strlen(s1) + 1; i++)
+	{
+		if (s1[i] != s2[i])
+			return false;
+	}
+	return true;
+}
+
+//CREATE TABLE table_name (
+
+bool valideaza(char* comanda)
+{
+	int l = lungimeComanda(comanda);
+	char* c = new char[100];
+	char* copie = new char[strlen(comanda) + 1];
+	strcpy_s(copie, strlen(comanda) + 1, comanda);
+	copie[l + 1] = '\0';
+
+	strcpy_s(c, strlen("CREATE TABLE") + 1, "CREATE TABLE\0");
+
+	if (strcmp(copie,c) == 0)
+	{
+		cout << "create";
+		return true;
+	}
+
+	strcpy_s(c, strlen("DROP TABLE") + 1, "DROP TABLE\0");
+
+	if (strcmp(copie,c) == 0)
+	{
+		cout << "drop";
+		return true;
+	}
+
+	strcpy_s(c, strlen("DISPLAY TABLE") + 1, "DISPLAY TABLE\0");
+
+	if (strcmp(copie, c) == 0)
+	{
+		cout << "display";
+		return true;
+	}
+
+	strcpy_s(c, strlen("CREATE INDEX") + 1, "CREATE INDEX\0");
+
+	if (strcmp(copie, c) == 0)
+	{
+		cout << "create index";
+		return true;
+	}
+
+	strcpy_s(c, strlen("DROP INDEX") + 1, "DROP INDEX\0");
+
+	if (strcmp(copie, c) == 0)
+	{
+		cout << "drop index";
+		return true;
+	}
+
+	return false;
+}
--- Proiect_POO.cpp
+++ Proiect_POO.cpp
@@ -1,20 +1,15 @@
-// Proiect_POO.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cstring>
+using namespace std;
+#include "Header.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char* sir = new char[100];
+    strcpy_s(sir, strlen("CREATE TABLE a") + 1, "CREATE TABLE a\0");
+    //strcpy_s(sir, strlen("DROP TABLE a") + 1, "DROP TABLE a\0");
+    //strcpy_s(sir, strlen("DISPLAY TABLE a") + 1, "DISPLAY TABLE a\0");
+    //strcpy_s(sir, strlen("CREATE INDEX a") + 1, "CREATE INDEX a\0");
+    //strcpy_s(sir, strlen("DROP INDEX a") + 1, "DROP INDEX a\0");
+    cout << valideaza(sir);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
